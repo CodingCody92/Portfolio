575e71ce88f4999ca8f7d0ade192ca6a3debc6ba
Cody <alexhill318@yahoo.com>
2022-01-31 12:29:28 -0600

    new changes

--- contact.php
+++ contact.php
@@ -1,20 +1,11 @@
+ 
+
 <?php
+echo "<pre>"
 
+print_r($_POST)
 
+echo "</pre>"
 
-if(isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailForm = $_POST['email'];
-    $message = $_POST['message'];
-
-    $mailTo = "codyalexanderhill@gmail.com, chill31806@yahoo.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have a message " .$name"./n/n".$message;
-
-    mail($mailTo, $name, $txt, $headers);
-
-    header("Location: index.html?MessageSent");
-
-}
 
 ?>
